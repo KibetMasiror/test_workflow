414b43305ed663dcc4c5883c3ae6802f09cbfff8
kibet masiror <32261713+MicroTot@users.noreply.github.com>
2023-06-14 07:54:02 +0300

    actions17

---
+++ cypress/e2e/foodie-pickup.cy.ts
@@ -0,0 +1,13 @@
+describe('Entering a delivery address', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:4200/')
+    cy.get('.test').type("HELLOOOOOOOOOOOO")
+  })
+
+describe('Add items to cart', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:4200/')
+    cy.get('.test').type("HELLOOOOOOOOOOOO")
+  })
+})
+})
